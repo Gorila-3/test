7c06294ffc1889610c58001a702e0e5181f5e86d
EC2 Default User <ec2-user@ip-172-31-61-195.ec2.internal>
2021-02-15 06:00:29 +0000

    テーブル制作2

--- db/migrate/20210215044736_create_cart_items.rb
+++ db/migrate/20210215044736_create_cart_items.rb
@@ -4,7 +4,7 @@ class CreateCartItems < ActiveRecord::Migration[5.0]
       t.integer :customer_id
       t.integer :item_id
       t.integer :amount
-　　　t.timestamps
+      t.timestamps
     end
   end
 end
