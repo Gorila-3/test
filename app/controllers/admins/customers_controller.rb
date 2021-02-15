cbf35e278eda68af1690ea9bfe2ba42c986245a9
EC2 Default User <ec2-user@ip-172-31-32-175.ec2.internal>
2021-02-15 07:36:55 +0000

    管理者側コントローラー作成

---
+++ app/controllers/admins/customers_controller.rb
@@ -0,0 +1,13 @@
+class Admins::CustomersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,93 @@
 Rails.application.routes.draw do
 
+  namespace :admins do
+    get 'order_details/update'
+  end
+
+  namespace :admins do
+    get 'orders/index'
+  end
+
+  namespace :admins do
+    get 'orders/show'
+  end
+
+  namespace :admins do
+    get 'orders/update'
+  end
+
+  namespace :admins do
+    get 'customers/index'
+  end
+
+  namespace :admins do
+    get 'categories/index'
+  end
+
+  namespace :admins do
+    get 'categories/create'
+  end
+
+  namespace :admins do
+    get 'categories/edit'
+  end
+
+  namespace :admins do
+    get 'categories/update'
+  end
+
+  namespace :admins do
+    get 'items/create'
+  end
+
+  namespace :admins do
+    get 'items/new'
+  end
+
+  namespace :admins do
+    get 'items/edit'
+  end
+
+  namespace :admins do
+    get 'items/update'
+  end
+
+  namespace :admins do
+    get 'items/index'
+  end
+
+  namespace :admins do
+    get 'items/show'
+  end
+
+  namespace :admins do
+    get 'items/top'
+  end
+
+  namespace :admins do
+    get 'items/about'
+  end
+
+  namespace :admins do
+    get 'customers/show'
+  end
+
+  namespace :admins do
+    get 'customers/out_show'
+  end
+
+  namespace :admins do
+    get 'customers/out'
+  end
+
+  namespace :admins do
+    get 'customers/edit'
+  end
+
+  namespace :admins do
+    get 'customers/update'
+  end
+
   devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
