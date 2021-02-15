8741f219f5b0d520874b149e8c193bf970c029a4
YusukeNaito08 <yusuke0208na@gmail.com>
2021-02-15 07:39:39 +0000

    [コントローラー追加]

---
+++ app/controllers/customers/cart_items_controller.rb
@@ -0,0 +1,16 @@
+class Customers::CartItemsController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def all_destroy
+  end
+
+  def update
+  end
+end
